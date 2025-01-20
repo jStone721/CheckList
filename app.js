2a87bf18a80121d7f80f43a5b6c60f5dcd765b63
Katherine Argall <k_argall@fanshawec.ca>
2025-01-19 22:34:02 -0500

    New commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-// The Firebase Admin SDK to access Firestore.
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 import { doc, setDoc, getDoc, getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
+
+const sw = new URL('service-worker.js', import.meta.url)
+if ('serviceWorker' in navigator) {
+    const s = navigator.serviceWorker;
+    s.register(sw.href, {
+        scope: '/CheckList/'
+    })
+        .then(_ => console.log('Service Worker Registered for scope:', sw.href, 'with', import.meta.url))
+        .catch(err => console.error('Service Worker Error:', err));
+}
 
 const firebaseConfig = {
     apiKey: "AIzaSyCuWLYWUJ2R4v6ptAKcL2jmXJPDSGl7uW0",
@@ -9,16 +19,19 @@ const firebaseConfig = {
     storageBucket: "info5146.firebasestorage.app",
     messagingSenderId: "711911517967",
     appId: "1:711911517967:web:cadd485436fb294a85987a"
-  };
+};
   
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
 const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
-getTasksFromFirestore();
+document.addEventListener('onload', () => {
+  renderTasks();
+});
 
 // Add Task
 addTaskBtn.addEventListener('click', async () => {
@@ -32,8 +45,10 @@ addTaskBtn.addEventListener('click', async () => {
             renderTasks();
             taskInput.value = "";
         }
+        renderTasks();
     }
 });
+
 
 async function renderTasks() {
     const tasks = await getTasksFromFirestore();
@@ -47,14 +62,14 @@ async function renderTasks() {
   }
 
   async function addTaskToFirestore(taskText) {
-    await setDoc(doc(db, "todos", "user1"), {
+    await setDoc(doc(db, "todos"), {
       text: taskText, 
       completed: false
     });  
   }
 
   async function getTasksFromFirestore() {
-    const userDoc = doc(db, "todos", "user1");
+    const userDoc = doc(db, "todos");
     const userData = (await getDoc(userDoc)).data();
   
     return userData?.tasks || [];
